412654e3eaefa470e335999c3f3f1fa6adb2bb39
Madmkisii <138007129+Madmkisii@users.noreply.github.com>
2024-04-10 02:25:32 +0300

    Update quotely.js

--- commands/edit/quotely.js
+++ commands/edit/quotely.js
@@ -3,14 +3,14 @@ import axios from "axios";
 const quotelyY = async (client, text, m, pushname) => {
     if (!text) return m.reply("provide some text for quotely");
 
-    const avatar = 'https://telegra.ph/file/b34645ca1e3a34f1b3978.jpg'
+    const avatar = 'https://telegra.ph/file/844ad6c0c9db3b78e5901.jpg'
 
     const imageUrl = `https://aemt.me/quotely?avatar=${avatar}&name=${pushname}&text=${text}`;
 
     try {
         const response = await axios.get(imageUrl, { responseType: 'arraybuffer' });
         const imageBuffer = Buffer.from(response.data, 'binary');
-        await client.sendImageAsSticker(m.chat, imageBuffer, m, { packname: pushname, author: "dreaded" });
+        await client.sendImageAsSticker(m.chat, imageBuffer, m, { packname: pushname, author: "𝐒𝐀𝐒𝐊𝐄" });
     } catch (error) {
         console.error('Error fetching image:', error);
     }
