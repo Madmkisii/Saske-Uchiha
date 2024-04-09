3c2f2d4775b7dfb0923a2a28af64adbb8b5a7edf
Madmkisii <138007129+Madmkisii@users.noreply.github.com>
2024-04-10 01:58:18 +0300

    Update screenshot.js

--- commands/general/screenshot.js
+++ commands/general/screenshot.js
@@ -12,7 +12,7 @@ let imageUrl = `https://aemt.me/sstab?url=${text}`
 await sendd("wait a moment")
 
 
-await client.sendMessage(m.chat, { image: { url: imageUrl}, caption: `! Screenshot by Dreaded! 🦄`}, { quoted: m}) 
+await client.sendMessage(m.chat, { image: { url: imageUrl}, caption: `! Screenshot by Saske𓃵𖤍🔥³`}, { quoted: m}) 
                
 
 
