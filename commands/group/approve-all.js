35c0ddacccbca1a1392928ba5d5780dfccf88333
Madmkisii <138007129+Madmkisii@users.noreply.github.com>
2024-04-10 02:12:38 +0300

    Update approve-all.js

--- commands/group/approve-all.js
+++ commands/group/approve-all.js
@@ -16,7 +16,7 @@ for (const participan of responseList) {
     );
     console.log(response);
 }
-m.reply("all pending participants have been approved 𓅓.");
+m.reply("all pending participants have been approved.");
 
 }
 
