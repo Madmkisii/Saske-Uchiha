956d2b8069df73dc6ab28df929eb6ce063ee7d36
Madmkisii <138007129+Madmkisii@users.noreply.github.com>
2024-04-10 02:26:54 +0300

    Update zombie.js

--- commands/edit/zombie.js
+++ commands/edit/zombie.js
@@ -19,7 +19,7 @@ await fetch(`https://aemt.me/converter/zombie?url=${fta}`)
     res = data.url;
 })
 
-await client.sendMessage(m.chat, { image: { url: res}, caption: "Converted by Owl 𓅓"}, { quoted: m});
+await client.sendMessage(m.chat, { image: { url: res}, caption: "Converted by Saske 𓃵"}, { quoted: m});
 
 }
 
